ca0536142338c8757f2b0018a7b12d52a1806abd
rakesh90084 <90084raky@gmail.com>
2021-07-22 00:33:45 +0530

    pincode

---
+++ pincode.py
@@ -0,0 +1,7 @@
+pincode=(560048,698989,404000,560036,540509,345678,696999,345554)
+print(pincode)
+print(tuple(range(pincode(1,5))))
+print(tuple(sorted(pincode)))
+# print(tuple(sorted(pincode,reverse=True)))
+# pincode.sort(reverse=True)
+# print(pincode)
